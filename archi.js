46a92d0f8e2f331182650229f4b4faf1828a991a
Joel Ajiola <ajiolajoel@gmail.com>
2023-01-24 00:09:55 +0100

    This is the architecture project

---
+++ archi.js
@@ -0,0 +1,7 @@
+function revealMenu() {
+    document.getElementById("mySidepanel").style.width = "100%";
+  }
+  
+  function closeMenu() {
+    document.getElementById("mySidepanel").style.width = "0";
+  }
